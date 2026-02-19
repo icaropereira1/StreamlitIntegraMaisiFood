7c3ff87288893b8b4095aa413188dd2ffe63550b
icaropereira1 <xicaroestudos@gmail.com>
2026-02-19 20:04:10 -0300

    fix: modificando app.py para gerar linhas com cores diferentes

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def gerar_excel_em_memoria(df):
     workbook  = writer.book
     worksheet = writer.sheets['Cardapio']
 
-    formato_bloqueado = workbook.add_format({'locked': True, 'bg_color': '#f2f2f2'})
+    formato_bloqueado = workbook.add_format({'locked': True, 'bg_color': '#F4F9FF'})
     formato_liberado = workbook.add_format({'locked': False})
 
     colunas_para_bloquear = ["Nível", "Categoria", "Produto Pai", "Item / Opcional", "Status", "ID iFood"]
@@ -113,6 +113,16 @@ def gerar_excel_em_memoria(df):
             worksheet.set_column(col_num, col_num, max_len, formato_bloqueado)
         else:
             worksheet.set_column(col_num, col_num, max_len, formato_liberado)
+    
+    # APLICANDO O EFEITO ZEBRA 
+    formato_zebra = workbook.add_format({'bg_color': '#F2F2F2'}) # Tom cinza claro
+    
+    # Aplica a formatação condicional da linha 1 (abaixo do cabeçalho) até o final do DF
+    worksheet.conditional_format(1, 0, len(df), len(df.columns) - 1, {
+        'type': 'formula',
+        'criteria': '=MOD(ROW(), 2) = 0', # Lê-se: Se a linha for par, aplique a cor
+        'format': formato_zebra
+    })
 
     worksheet.autofilter(0, 0, len(df), len(df.columns) - 1)
     worksheet.protect('xicaroehfoda', {
